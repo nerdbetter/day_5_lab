a48203c3a9aa4051e47b9231b283f2ff6bf98b65
nerdbetter <jasontlogan@gmail.com>
2017-06-11 11:02:46 -0500

    commented out sections

--- app.js
+++ app.js
@@ -47,17 +47,17 @@ IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this funct
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a,b,c){ //eslint-disable-line
-  var sumTwo = sum(sum(a,b)[0], c)[0];
-  var multTwo = multiply(multiply(a,b)[0], c)[0];
-  var alltotalEquation = [ sumTwo, multTwo,
-    a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.',
-    'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multTwo + '.',];
-  return alltotalEquation;
-}
+//function sumAndMultiply(a,b,c){ //eslint-disable-line
+//  var sumTwo = sum(sum(a,b)[0], c)[0];
+  //var multTwo = multiply(multiply(a,b)[0], c)[0];
+  //var alltotalEquation = [ sumTwo, multTwo,
+    //a + ' and ' + b + ' and ' + c + ' sum to ' + sumTwo + '.',
+    //'The product of ' + a + ' and ' + b + ' and ' + c + ' is ' + multTwo + '.',];
+  //return alltotalEquation;
+//}
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-testSumAndMultiply(4,7,5);
+//testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
